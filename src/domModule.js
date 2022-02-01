6794cbfd5d1f869de62005ca464205a72f99f366
Jonathan Ng <jonathanng15@gmail.com>
2022-02-01 02:21:27 -0800

    Moved DOM logic from index.js to separate module

--- src/domModule.js
+++ src/domModule.js
@@ -1,24 +1,25 @@
-import Ship from './shipModule';
-import Gameboard from './gameboardModule';
-import Player from './playerModule';
+import Ship from "./shipModule";
+import Gameboard from "./gameboardModule";
+import Player from "./playerModule";
 
-const createDOMGrid = (container) => {
+export const createDOMGrid = (container, player) => {
   for (let i = 1; i <= 10; i++) {
     for (let j = 1; j <= 10; j++) {
-      const grid = document.createElement('div');
-      grid.setAttribute('xy-coord', `${j}-${i}`);
-      grid.addEventListener('dragover', (e) => {
+      const grid = document.createElement("div");
+      grid.setAttribute("xy-coord", `${j}-${i}`);
+      grid.addEventListener("dragover", e => {
         e.preventDefault();
       });
-      grid.addEventListener('drop', (e) => {
-        const shipName = e.dataTransfer.getData('ShipName');
-        const shipAxis = e.dataTransfer.getData('ShipAxis');
-        const ship = findShip(shipName);
-        const [xCoord, yCoord] = grid.getAttribute('xy-coord').split('-');
-        if (player1.gameboard.placeShip(ship, yCoord, xCoord, shipAxis)) {
-          const draggable = document.querySelector('.dragging');
+      grid.addEventListener("drop", e => {
+        const shipName = e.dataTransfer.getData("ShipName");
+        const shipAxis = e.dataTransfer.getData("ShipAxis");
+        const ship = findShip(shipName, player);
+        const [xCoord, yCoord] = grid.getAttribute("xy-coord").split("-");
+        if (player.gameboard.placeShip(ship, yCoord, xCoord, shipAxis)) {
+          const draggable = document.querySelector(".dragging");
           grid.appendChild(draggable);
         }
+        console.log(player);
       });
 
       container.appendChild(grid);
@@ -26,76 +27,70 @@ const createDOMGrid = (container) => {
   }
 };
 
-const findShip = (name) => {
-  const shipObj = player1.gameboard.ships.find((ship) => ship.name == name);
+export const findShip = (name, player) => {
+  const shipObj = player.gameboard.ships.find(ship => ship.name == name);
   return shipObj;
 };
 
-document.querySelectorAll('.drag-item').forEach((item) => {
-  item.addEventListener('dragstart', (e) => {
-    item.classList.add('dragging');
-    e.dataTransfer.setData('ShipName', e.target.id);
-    e.dataTransfer.setData('ShipAxis', e.target.getAttribute('data-axis'));
+document.querySelectorAll(".drag-item").forEach(item => {
+  item.addEventListener("dragstart", e => {
+    item.classList.add("dragging");
+    e.dataTransfer.setData("ShipName", e.target.id);
+    e.dataTransfer.setData("ShipAxis", e.target.getAttribute("data-axis"));
   });
 });
 
-document.querySelectorAll('.drag-item').forEach((item) => {
-  item.addEventListener('dragend', () => {
-    item.classList.remove('dragging');
+document.querySelectorAll(".drag-item").forEach(item => {
+  item.addEventListener("dragend", () => {
+    item.classList.remove("dragging");
   });
 });
 
-const colorAttackedCell = (x, y, gameboard, container) => {
+export const colorAttackedCell = (x, y, player, container) => {
   const selectedCell = document.querySelector(
     `${container} [xy-coord="${x}-${y}"]`
   );
   const xCoord = x - 1;
   const yCoord = y - 1;
-  if (typeof gameboard.grid[yCoord][xCoord] == 'object') {
-    selectedCell.style.backgroundColor = 'green';
+  if (typeof player.gameboard.grid[yCoord][xCoord] == "object") {
+    selectedCell.style.backgroundColor = "green";
   } else {
     // Mark board as not hit
-    selectedCell.style.backgroundColor = 'yellow';
+    selectedCell.style.backgroundColor = "yellow";
   }
 };
 
-const rotateShips = () => {
-  const rotateBtn = document.querySelector('#rotate-btn');
-  if (rotateBtn.getAttribute('data-rotation') == 'x') {
+export const rotateShips = () => {
+  const rotateBtn = document.querySelector("#rotate-btn");
+  if (rotateBtn.getAttribute("data-rotation") == "x") {
     rotateY();
-    rotateBtn.setAttribute('data-rotation', 'y');
+    rotateBtn.setAttribute("data-rotation", "y");
   } else {
     rotateX();
-    rotateBtn.setAttribute('data-rotation', 'x');
+    rotateBtn.setAttribute("data-rotation", "x");
   }
 };
 
-const rotateY = () => {
-  const shipsContainer = document.querySelector('#ships-container');
-  shipsContainer.querySelectorAll('.drag-item').forEach((item) => {
+export const rotateY = () => {
+  const shipsContainer = document.querySelector("#ships-container");
+  shipsContainer.querySelectorAll(".drag-item").forEach(item => {
     const width = item.offsetWidth;
     item.style.height = `${width / 16}rem`;
     item.style.width = `2.5rem`;
-    item.style.flexDirection = 'column';
-    item.setAttribute('data-axis', 'y');
+    item.style.flexDirection = "column";
+    item.setAttribute("data-axis", "y");
   });
-  shipsContainer.style.flexDirection = 'row';
+  shipsContainer.style.flexDirection = "row";
 };
 
-const rotateX = () => {
-  const shipsContainer = document.querySelector('#ships-container');
-  shipsContainer.querySelectorAll('.drag-item').forEach((item) => {
+export const rotateX = () => {
+  const shipsContainer = document.querySelector("#ships-container");
+  shipsContainer.querySelectorAll(".drag-item").forEach(item => {
     const height = item.offsetHeight;
     item.style.height = `2.5rem`;
     item.style.width = `${height / 16}rem`;
-    item.style.flexDirection = 'row';
-    item.setAttribute('data-axis', 'x');
+    item.style.flexDirection = "row";
+    item.setAttribute("data-axis", "x");
   });
-  shipsContainer.style.flexDirection = 'column';
+  shipsContainer.style.flexDirection = "column";
 };
-
-document.querySelector('#rotate-btn').addEventListener('click', () => {
-  rotateShips();
-});
-
-export { createDOMGrid, colorAttackedCell };
--- src/index.js
+++ src/index.js
@@ -1,118 +1,17 @@
-'use strict';
+"use strict";
 
-import Ship from './shipModule';
-import Gameboard from './gameboardModule';
-import Player from './playerModule';
+import Ship from "./shipModule";
+import Gameboard from "./gameboardModule";
+import * as DOM from "./domModule";
+import Player from "./playerModule";
 
-const player1 = new Player('Jon');
-const player2 = new Player('CPU');
+const player1 = new Player("Jon");
+const player2 = new Player("CPU");
 player2.randomShipPlacement();
 
-const createDOMGrid = (container) => {
-  for (let i = 1; i <= 10; i++) {
-    for (let j = 1; j <= 10; j++) {
-      const grid = document.createElement('div');
-      grid.setAttribute('xy-coord', `${j}-${i}`);
-      grid.addEventListener('dragover', (e) => {
-        e.preventDefault();
-      });
-      grid.addEventListener('drop', (e) => {
-        const shipName = e.dataTransfer.getData('ShipName');
-        const shipAxis = e.dataTransfer.getData('ShipAxis');
-        const ship = findShip(shipName);
-        const [xCoord, yCoord] = grid.getAttribute('xy-coord').split('-');
-        if (player1.gameboard.placeShip(ship, yCoord, xCoord, shipAxis)) {
-          const draggable = document.querySelector('.dragging');
-          grid.appendChild(draggable);
-        }
-        console.log(player1);
-      });
+DOM.createDOMGrid(document.querySelector("#gameboard-1"), player1);
+DOM.createDOMGrid(document.querySelector("#gameboard-2"));
 
-      container.appendChild(grid);
-    }
-  }
-};
-
-const findShip = (name) => {
-  const shipObj = player1.gameboard.ships.find((ship) => ship.name == name);
-  return shipObj;
-};
-
-document.querySelectorAll('.drag-item').forEach((item) => {
-  item.addEventListener('dragstart', (e) => {
-    item.classList.add('dragging');
-    e.dataTransfer.setData('ShipName', e.target.id);
-    e.dataTransfer.setData('ShipAxis', e.target.getAttribute('data-axis'));
-  });
+document.querySelector("#rotate-btn").addEventListener("click", () => {
+  DOM.rotateShips();
 });
-
-document.querySelectorAll('.drag-item').forEach((item) => {
-  item.addEventListener('dragend', () => {
-    item.classList.remove('dragging');
-  });
-});
-// document.querySelector('#drag-item').addEventListener('dragstart', () => {
-//   document.querySelector('#drag-item').classList.add('dragging');
-// });
-
-// document.querySelector('#drag-item').addEventListener('dragend', () => {
-//   document.querySelector('#drag-item').classList.remove('dragging');
-// });
-
-const colorAttackedCell = (x, y, gameboard, container) => {
-  const selectedCell = document.querySelector(
-    `${container} [xy-coord="${x}-${y}"]`
-  );
-  const xCoord = x - 1;
-  const yCoord = y - 1;
-  if (typeof gameboard.grid[yCoord][xCoord] == 'object') {
-    selectedCell.style.backgroundColor = 'green';
-  } else {
-    // Mark board as not hit
-    selectedCell.style.backgroundColor = 'yellow';
-  }
-};
-
-createDOMGrid(document.querySelector('#gameboard-1'));
-createDOMGrid(document.querySelector('#gameboard-2'));
-
-const rotateShips = () => {
-  const rotateBtn = document.querySelector('#rotate-btn');
-  if (rotateBtn.getAttribute('data-rotation') == 'x') {
-    rotateY();
-    rotateBtn.setAttribute('data-rotation', 'y');
-  } else {
-    rotateX();
-    rotateBtn.setAttribute('data-rotation', 'x');
-  }
-};
-
-const rotateY = () => {
-  const shipsContainer = document.querySelector('#ships-container');
-  shipsContainer.querySelectorAll('.drag-item').forEach((item) => {
-    const width = item.offsetWidth;
-    item.style.height = `${width / 16}rem`;
-    item.style.width = `2.5rem`;
-    item.style.flexDirection = 'column';
-    item.setAttribute('data-axis', 'y');
-  });
-  shipsContainer.style.flexDirection = 'row';
-};
-
-const rotateX = () => {
-  const shipsContainer = document.querySelector('#ships-container');
-  shipsContainer.querySelectorAll('.drag-item').forEach((item) => {
-    const height = item.offsetHeight;
-    item.style.height = `2.5rem`;
-    item.style.width = `${height / 16}rem`;
-    item.style.flexDirection = 'row';
-    item.setAttribute('data-axis', 'x');
-  });
-  shipsContainer.style.flexDirection = 'column';
-};
-
-document.querySelector('#rotate-btn').addEventListener('click', () => {
-  rotateShips();
-});
-
-console.log(player2);
